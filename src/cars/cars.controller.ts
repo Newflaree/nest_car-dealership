eac9d492cd34dc0fd96acf394801fc011e8312fc
Camilo López <camiloj.lopez79@gmail.com>
2025-02-12 19:26:06 -0300

    Controllers

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+
+@Controller('cars')
+export class CarsController {
+  @Get()
+  getAllCars() {
+    return [
+      'Toyota',
+      'Honda',
+      'Jeep',
+    ]
+  }
+}
