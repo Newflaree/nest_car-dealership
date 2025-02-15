bf51aeebaba3bbd539fe098c7b4b0ff3dbccd925
Camilo López <camiloj.lopez79@gmail.com>
2025-02-14 22:07:58 -0300

    Post, Patch, Delete

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,13 @@
 // Nest
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   Param,
-  ParseIntPipe
+  ParseIntPipe,
+  Patch,
+  Post
 } from '@nestjs/common';
 // Services
 import { CarsService } from './cars.service';
@@ -24,5 +28,26 @@ export class CarsController {
   getCarById( @Param( 'id', ParseIntPipe ) id: number ) {
     console.log({ id });
     return this.carsService.findOneById( id );
+  }
+
+  @Post()
+  createCar( @Body() body: any ) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(
+    @Param( 'id', ParseIntPipe ) id: number,
+    @Body() body: any
+  ) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar( @Param( 'id', ParseIntPipe ) id: number ) {
+    return {
+      method: 'delete',
+      id
+    }
   }
 }
